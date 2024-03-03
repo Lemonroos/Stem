2a4cb45f5513990bfd00aacaf1eb23fc9a538bba
Nguyen Anh Duy <aduy1912@gmail.com>
2024-03-03 23:54:51 +0700

    add labs in my programs-detail

---
+++ src/Components/models/Labs.tsx
@@ -0,0 +1,12 @@
+export interface Labs{
+    Id: number,
+    Code: string,
+    Topic: string,
+    Description: string,
+    CreatedDate: string,
+    EndDate: string,
+    StartDate: string,
+    UpdatedDate: string,
+    ProgramId: number,
+    ProgramName: string
+}
