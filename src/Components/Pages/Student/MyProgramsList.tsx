5c406f9659aa6c25e5dbea3f058718f06757e724
Nguyen Anh Duy <aduy1912@gmail.com>
2024-03-16 12:00:58 +0700

    add enroll program for Student

--- src/Components/Pages/Student/MyProgramsList.tsx
+++ src/Components/Pages/Student/MyProgramsList.tsx
@@ -3,14 +3,15 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import getUser from "../../../config/auth";
-import { ProgramAndGroup } from "../../models/Programs";
 import MySpin from "../../UI/spin";
+import { ProgramsOfAMember } from "../../models/Programs";
 
 const { Meta } = Card;
 
 const MyProgramList = () => {
+    const progamsByStudentUrl = 'https://stem-backend.vercel.app/api/v1/members/programs-of-a-student';
     const [userId, setUserId] = useState<any>(null);
-    const [myPrograms, setMyPrograms] = useState<ProgramAndGroup[]>([]);
+    const [myPrograms, setMyPrograms] = useState<ProgramsOfAMember[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         const fetchUser = async () => {
@@ -23,7 +24,6 @@ const MyProgramList = () => {
     }, []);
     // const studentId = userId;
     console.log(userId);
-    const progamsByStudentUrl = 'https://stem-backend.vercel.app/api/v1/members/programs-of-a-student';
     async function getProgramByStudentId() {
         try {
             await axios.get(`${progamsByStudentUrl}?StudentId=${userId}`)
