48d5bc305c0bbde8ff72acefbab2ff591dd22958
Lemonroos <codeassasault@gmail.com>
2024-03-20 00:31:34 +0700

    fix error

--- src/Components/Pages/Student/LabDetails.tsx
+++ src/Components/Pages/Student/LabDetails.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, List, Row, Tabs } from "antd";
+import { Card, Col, Row, Tabs } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
